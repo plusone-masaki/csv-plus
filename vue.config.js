04f08d3c4b9cfe2d84b33f15e708fdbbc0351480
Masaki Enomoto <m_enomoto@plus-one.tech>
2021-02-02 13:23:57 +0900

    # ビルド設定の修正 - インストーラ形式で出力するように変更 - Mac版ではファイルの関連付け設定

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,27 @@ module.exports = {
       mainProcessFile: 'src/main/index.ts',
       rendererProcessFile: 'src/renderer/index.ts',
       nodeIntegration: true,
+      builderOptions: {
+        mac: {
+          category: 'public.app-category.developer-tools',
+          target: [
+            'dmg',
+          ],
+          extendInfo: {
+            CFBundleDocumentTypes: [
+              {
+                CFBundleTypeExtensions: ['csv', 'tsv'],
+                // CFBundleTypeIconFile: 'My Awesome Application.icns',
+              },
+            ],
+          },
+        },
+        linux: {
+          target: [
+            'deb',
+          ],
+        },
+      },
     },
   },
 }
