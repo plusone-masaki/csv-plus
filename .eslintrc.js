f76be4c28bb33e387bf3ba6f610313bedfeef64c
Masaki Enomoto <m_enomoto@plus-one.tech>
2021-06-22 14:02:13 +0900

    # Cmdキーのショートカットに対応
    - HandsOnTable をライセンスフリーのバージョンにダウングレード
    - ショートカットキーの登録処理を一元化
        - これは将来的にキーコンフィグ機能の実装に役立ちます
    - いくつかのショートカットキーをMac用に最適化
    - Ctrl+Zで「元に戻す」機能が2回発生する不具合の解消

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,7 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-non-null-assertion': 'off',
     'comma-dangle': ['error', 'always-multiline'],
   },
   overrides: [
--- src/renderer/components/Grids/composables/types.d.ts
+++ src/renderer/components/Grids/composables/types.d.ts
@@ -1,10 +1,7 @@
-import HandsOnTable from 'handsontable'
-import { Calculation, FileData, Options } from '@/common/types'
-
 export type Props = {
-  table: HandsOnTable | null;
-  file: FileData;
-  keyword: string;
-  options: Options;
-  calculation: Calculation;
+  table: Table | null
+  file: FileData
+  keyword: string
+  options: Options
+  calculation: Calculation
 }
