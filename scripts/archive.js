18627132643578fc400bc26358d68e6d34d97dff
m_enomoto <m_enomoto@plus-one.tech>
2021-08-01 15:52:32 +0900

    # インストーラのzip配布ができていない不具合を修正 - ElectronBuilderを解析し、正しくPromiseが解決されるよう修正

--- scripts/archive.js
+++ scripts/archive.js
@@ -12,35 +12,37 @@ const archiver = require('archiver')
 module.exports = async buildResult => {
   const winInstaller = buildResult.artifactPaths.find(filePath => /setup.*\.exe$/i.test(filePath))
   if (winInstaller) {
-    const filepath = winInstaller.replace('exe', 'zip')
-    const output = fs.createWriteStream(filepath)
-    const archive = archiver('zip')
+    return new Promise(resolve => {
+      const filepath = winInstaller.replace('exe', 'zip')
+      const output = fs.createWriteStream(filepath)
+      const archive = archiver('zip')
 
-    output.on('close', () => {
-      console.log(`${archive.pointer()} total bytes`)
-      console.log('archiver has been finalized and the output file descriptor has closed.')
-    })
+      output.on('close', () => {
+        console.log(`${archive.pointer()} total bytes`)
+        console.log('archiver has been finalized and the output file descriptor has closed.')
+        resolve([filepath])
+      })
 
-    output.on('end', () => {
-      console.log('Data has been drained')
-      // TODO: 将来的には元exeをアップロード対象から外す
-    })
+      output.on('end', () => {
+        console.log('Data has been drained')
+        // TODO: 将来的には元exeをアップロード対象から外す
+      })
 
-    archive.on('warning', err => {
-      if (err.code === 'ENOENT') {
-        // log warning
-      } else {
-        // throw error
+      archive.on('warning', err => {
+        if (err.code === 'ENOENT') {
+          // log warning
+        } else {
+          // throw error
+          throw err
+        }
+      })
+
+      archive.on('error', err => {
         throw err
-      }
-    })
+      })
 
-    archive.on('error', err => {
-      throw err
+      archive.pipe(output)
+      archive.file(winInstaller, { name: winInstaller.split(path.sep).pop() }).finalize()
     })
-
-    archive.pipe(output)
-    await archive.file(winInstaller, { name: winInstaller.split(path.sep).pop() }).finalize()
-    return [filepath]
   }
 }
