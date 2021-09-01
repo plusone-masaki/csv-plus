54668dbaf304d917614bc15c8468c24fdebc06c4
m_enomoto <m_enomoto@plus-one.tech>
2021-09-01 11:05:20 +0900

    - コンフリクト修正

--- src/renderer/components/Grids/composables/gridHooks.ts
+++ src/renderer/components/Grids/composables/gridHooks.ts
@@ -140,20 +140,6 @@ export default (props: { tab: Tab }, context: SetupContext) => ({
     })
   },
 
-  afterSelection: (startRow: number, startCol: number, endRow: number, endCol: number) => {
-    if (!props.tab.table.instance) return
-
-    const rowLength = endRow - startRow
-    const colLength = endCol - startCol
-
-    const values = props.tab.table.instance.getData(startRow, startCol, endRow, endCol).flat() as string[]
-    props.tab.calculation.selected = {
-      rowLength,
-      colLength,
-      summary: (rowLength || colLength) && values.reduce((a, b) => a + Number(b), 0),
-    }
-  },
-
   beforeCopy: (data: string[][]) => {
     // 設定された区切り文字・改行コードでデータをコピー
     if (navigator.clipboard) {
