53ddfaa19f36687cf9d05d24949f83df3a4d8b34
m_enomoto <m_enomoto@plus-one.tech>
2022-03-29 22:57:40 +0900

    # ユニットテスト作成 - CSV/TSV/パイプ区切り(PSV)の読み込みテスト - 各エンコーディングの読み込みテストを網羅 - 「最近開いたファイル」「最後に開いたフォルダ」機能のテストを作成

--- tests/unit/main/modules/History.test.ts
+++ tests/unit/main/modules/History.test.ts
@@ -7,17 +7,17 @@ let history: History
 
 describe('Module: [History]', () => {
   beforeAll(() => {
-    fs.mkdirSync(path.join(os.tmpdir(), 'test', 'userData', 'history'), { recursive: true })
-    fs.writeFileSync(path.join(os.tmpdir(), 'test', 'userData', 'history', 'recent-directory'), '')
-    fs.writeFileSync(path.join(os.tmpdir(), 'test', 'userData', 'history', 'recent-documents.json'), '[]')
-    fs.writeFileSync(path.join(os.tmpdir(), 'test', 'userData', 'history', 'tab-history.json'), '[]')
+    fs.mkdirSync(path.resolve(os.tmpdir(), 'test', 'userData', 'history'), { recursive: true })
+    fs.writeFileSync(path.resolve(os.tmpdir(), 'test', 'userData', 'history', 'recent-directory'), '')
+    fs.writeFileSync(path.resolve(os.tmpdir(), 'test', 'userData', 'history', 'recent-documents.json'), '[]')
+    fs.writeFileSync(path.resolve(os.tmpdir(), 'test', 'userData', 'history', 'tab-history.json'), '[]')
   })
   afterAll(() => {
     fs.rmSync(path.join(os.tmpdir(), 'test', 'userData', 'history'), { recursive: true })
   })
 
   describe('Directory', () => {
-    const filepath = path.join(os.tmpdir(), 'test', 'userData', 'history', 'recent-directory')
+    const filepath = path.resolve(os.tmpdir(), 'test', 'userData', 'history', 'recent-directory')
 
     describe('Default path', () => {
       beforeAll(() => {
@@ -26,22 +26,66 @@ describe('Module: [History]', () => {
       })
 
       test('Default path is "Documents".', () => {
-        expect(history.recentDirectory).toBe(path.join(os.tmpdir(), 'test', 'documents'))
+        expect(history.recentDirectory).toBe(path.resolve(os.tmpdir(), 'test', 'documents'))
       })
     })
 
     describe('Set path.', () => {
       beforeAll(() => {
-        fs.writeFileSync(filepath, path.join(os.tmpdir(), 'test', 'some/filepath.csv'))
+        fs.writeFileSync(filepath, path.resolve(os.tmpdir(), 'test', 'some', 'filepath.csv'))
         history = new History()
       })
 
       test('The set path is read.', () => {
-        expect(history.recentDirectory).toBe(path.join(os.tmpdir(), 'test', 'some/filepath.csv'))
+        expect(history.recentDirectory).toBe(path.resolve(os.tmpdir(), 'test', 'some', 'filepath.csv'))
+      })
+    })
+
+    describe('Persistent path.', () => {
+      const data = path.resolve(os.tmpdir(), 'path', 'to', 'hoge.csv')
+      beforeAll(() => {
+        fs.writeFileSync(filepath, '')
+        history = new History()
+        history.recentDirectory = data
+        history.persistentRecentDirectory()
+      })
+
+      test('The set path is written.', () => {
+        expect(fs.readFileSync(filepath, { encoding: 'utf8' })).toBe(data)
       })
     })
   })
 
-  // TODO: recent-documents
+  describe('Recent documents', () => {
+    const filepath = path.resolve(os.tmpdir(), 'test', 'userData', 'history', 'recent-documents.json')
+
+    describe('Default', () => {
+      beforeAll(() => {
+        fs.writeFileSync(filepath, '')
+        history = new History()
+      })
+
+      test('Default "recentDocuments" is empty array.', () => {
+        expect(history.recentDocuments).toStrictEqual([])
+      })
+    })
+
+    describe('Set path.', () => {
+      const data = [
+        path.resolve(os.tmpdir(), 'test', 'some/hoge.csv'),
+        path.resolve(os.tmpdir(), 'test', 'some/fuga.csv'),
+        path.resolve(os.tmpdir(), 'test', 'some/moge.csv'),
+      ]
+
+      beforeAll(() => {
+        fs.writeFileSync(filepath, JSON.stringify(data))
+        history = new History()
+      })
+
+      test('The set path is written.', () => {
+        expect(history.recentDocuments).toStrictEqual(data)
+      })
+    })
+  })
   // TODO: tab-history
 })
